f9db392104a30f9316867965e6fd2dca2f70db82
frontendedy <frontendedy@gmail.com>
2023-07-16 11:11:57 +0530

    Added github link

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './style.css'
-import { MdOutlineFileDownload} from 'react-icons/md';
+import { MdOutlineFileDownload } from 'react-icons/md';
+import { AiFillGithub } from 'react-icons/ai';
 
-const TopNav = ({downloadImage}) => {
+const TopNav = ({ downloadImage }) => {
   return (
     <nav className='navContainer'>
-        <div className='navInner'>
-            <div className="logo">Social <span>Post</span></div>
-            <div className="downloadBtn" onClick={downloadImage}><MdOutlineFileDownload size={25}/>Download</div>
+      <div className='navInner'>
+        <div className="logo">Social <span>Post</span>
+          <a href="https://github.com/LalitKumar234/Social-Post-Generator" target='_blank'>
+            <AiFillGithub color='black'/>
+          </a>
         </div>
+        <div className="downloadBtn" onClick={downloadImage}><MdOutlineFileDownload size={25} />Download</div>
+      </div>
     </nav>
   )
 }
